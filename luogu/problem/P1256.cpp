c0fd81dfeb4556535422b5144a0b8919ad90de00
Transparent-fish <luogu_dnj@outlook.com>
2026-04-07 22:14:49 +0800

    update

---
+++ luogu/problem/P1256.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N = 205;
+int n, m;
+char s[N][N];
+
+signed main() {
+    cin >> n >> m;
+    for (int i = 1;i <= n;i++) {
+        for (int j = 1;j <= m;j++) {
+
+        }
+    }
+    return 0;
+}
